398da31d60f16a1d6cfbc1c02d0a54c9e73d2344
Magi Wanders <magi.wanders@gmail.com>
2021-04-02 12:38:17 +0200

    added parallel tqdm

--- useful_snippets/parallel_computing_tqdm_demo.py
+++ useful_snippets/parallel_computing_tqdm_demo.py
@@ -1,7 +1,7 @@
-from multiprocessing import Pool
 import numpy as np
 import time
 from tqdm import tqdm
+from p_tqdm import p_map
 
 # This contains all the inputs to the function that can be parallelized. This example parallelizes 300 instances of an expensive function. As the expensive data takes 0.01 seconds, the WCS is 3 seconds + overhead.
 inputs = np.random.rand(300)
@@ -15,14 +15,14 @@ t1 = time.time()
 for i in tqdm(inputs):
     expensive_parallelizable_task(i)
 
-t2 = time.time() 
+t2 = time.time()
 print('Traditional computing time: ' + str(t2-t1))
 
 # /////////////////////////////////// PARALLEL COMPUTING
 t1 = time.time()
 
-with Pool() as p: # Pool(n) forces n threads
-        p.map(expensive_parallelizable_task, inputs)
+if __name__ == '__main__':
+    p_map(expensive_parallelizable_task, inputs)
 
 t2 = time.time()
 print('Parallel computing time: ' + str(t2-t1))
