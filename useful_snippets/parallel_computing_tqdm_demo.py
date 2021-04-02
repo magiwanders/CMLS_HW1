985fcc38556790f13080a77acf97495ed57e993d
Magi Wanders <magi.wanders@gmail.com>
2021-04-02 13:29:32 +0200

    Added result comparison

--- useful_snippets/parallel_computing_tqdm_demo.py
+++ useful_snippets/parallel_computing_tqdm_demo.py
@@ -4,16 +4,19 @@ from tqdm import tqdm
 from p_tqdm import p_map
 
 # This contains all the inputs to the function that can be parallelized. This example parallelizes 300 instances of an expensive function. As the expensive data takes 0.01 seconds, the WCS is 3 seconds + overhead.
-inputs = np.random.rand(300)
+inputs = np.random.rand(3000)
+out_trad = []
+out_parall = []
 
 def expensive_parallelizable_task(x):
     time.sleep(0.01) # Very expensive! Takes 0.01 seconds
+    return x*x
 
 # /////////////////////////////////// TRADITIONAL COMPUTING
 t1 = time.time()
 
 for i in tqdm(inputs):
-    expensive_parallelizable_task(i)
+    out_trad.append(expensive_parallelizable_task(i))
 
 t2 = time.time()
 print('Traditional computing time: ' + str(t2-t1))
@@ -22,7 +25,12 @@ print('Traditional computing time: ' + str(t2-t1))
 t1 = time.time()
 
 if __name__ == '__main__':
-    p_map(expensive_parallelizable_task, inputs)
+    out_parall = p_map(expensive_parallelizable_task, inputs)
 
 t2 = time.time()
 print('Parallel computing time: ' + str(t2-t1))
+
+if out_trad == out_parall:
+    print('[SUCCESS] The two computations gave the same result')
+else:
+    print('[FAIL] Methods are not equivalent')
